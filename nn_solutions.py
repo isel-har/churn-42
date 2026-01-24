69ccdbc22a64a6daafb5384739801fff42a79567
Ismail El harrab <isel-har@e1r5p1.1337.ma>
2026-01-24 17:01:11 +0100

    preprocessing completed

---
+++ nn_solutions.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import joblib
+
+from sklearn.model_selection import train_test_split#, GridSearchCV
+from sklearn.metrics import accuracy_score, auc
+
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras import Sequential
+
+
